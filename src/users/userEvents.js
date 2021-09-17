1ca3961a4371c1600768269c3de0e27a0171e092
Siarhei Khonski <armonius@mail.ru>
2021-09-17 23:11:49 +0300

    feat: init

--- src/users/userEvents.js
+++ src/users/userEvents.js
@@ -1,54 +1,26 @@
-import {
-    CONNECTION,
-    DISCONNECT,
-    JOIN_ROOM,
-    SEND_MESSAGE,
-    RECEIVE_MESSAGE,
-    USER_CONNECTED,
-    USER_DISCONNECTED,
-  } from '../utils/constants';
-  import store from './users';
+import rooms from '../rooms/rooms';
 
-  export const userEvents =  (io) => {
-    io.on(CONNECTION, (socket) => {
-        let userId;
-        let roomToBroadcast;
-    
-        try {
-          socket.on(JOIN_ROOM,  ({ room, id, firstName, lastName }) => {
-            //здесь будет логика добавления пользователя в комнату
-            store.addUser({id, firstName, lastName, room});
-            userId = id;
-            socket.join(room);
-            roomToBroadcast = room;  
-            socket.to(roomToBroadcast).emit(USER_CONNECTED, userId);
-            io.to(roomToBroadcast).emit('SHOW_USERS', store.getUsers(roomToBroadcast));
-          })
+export const userEvents =  (io) => {
+  io.on('connection', (socket) => {
+    try {
+      socket.on('create_session', (data) => {
+      const {sessionName, user} = data;
+      if (rooms.has(sessionName)) { console.log(`ROOM ${sessionName} IS ALREADY CREATED!!!`) }
+      else if (socket.rooms.size < 2) {
+        user.id = socket.id;
+        const roomInfo = {users: [], cards: [], issues: []};
+        roomInfo.users.push(user);
+        rooms.set(sessionName, roomInfo);
+        socket.join(sessionName);
+        console.log(rooms.get(sessionName));
+      } else { console.log('YOU ARE IN THE ROOM! LEAVE IT AND TRY AGAIN!') }
+    })
 
-          socket.on('LEAVE_ROOM',  (id) => {
-            //здесь будет логика удаления пользователя из комнаты
-            const leavingUser = store.removeUser(id);
-            userId = leavingUser.id;
-            roomToBroadcast = leavingUser.room;  
-            socket.leave(leavingUser.room);
-            socket.to(roomToBroadcast).emit(USER_DISCONNECTED, userId);
-            io.to(roomToBroadcast).emit('SHOW_USERS', store.getUsers(roomToBroadcast));
-          })
+    } catch (error) {
+        console.log(error);
+      //return;
+    }
 
-    
-          socket.on(SEND_MESSAGE, (message) => {
-            io.to(roomToBroadcast).emit(RECEIVE_MESSAGE, { message, from: userId });
-          })
-    
-          socket.on(DISCONNECT, () => {
-            socket.to(roomToBroadcast).emit(USER_DISCONNECTED, userId);
-          })
-
-
-        } catch (error) {
-          console.log(error)
-        }
-
-      })
-  }
+ })
+}
   
