cecf514e2ba3103397fe5addf1595dce69decfb9
Siarhei Khonski <armonius@mail.ru>
2021-09-18 12:39:25 +0300

    feat: fill

--- src/users/userEvents.js
+++ src/users/userEvents.js
@@ -3,22 +3,38 @@ import rooms from '../rooms/rooms';
 export const userEvents =  (io) => {
   io.on('connection', (socket) => {
     try {
-      socket.on('create_session', (data) => {
+      socket.on('create_session', (data) => {  //создание новой игры
       const {sessionName, user} = data;
-      if (rooms.has(sessionName)) { console.log(`ROOM ${sessionName} IS ALREADY CREATED!!!`) }
-      else if (socket.rooms.size < 2) {
+      if (rooms.has(sessionName)) { console.log(`ROOM ${sessionName} IS ALREADY CREATED!!!`) }  //проверка имени комнаты
+      else if (socket.rooms.size < 2) {  //проверка что пользователь не находится в другой комнате
         user.id = socket.id;
+        user.status = 'connect'
         const roomInfo = {users: [], cards: [], issues: []};
         roomInfo.users.push(user);
         rooms.set(sessionName, roomInfo);
         socket.join(sessionName);
-        console.log(rooms.get(sessionName));
       } else { console.log('YOU ARE IN THE ROOM! LEAVE IT AND TRY AGAIN!') }
     })
 
+      socket.on('join_game', (data) => {   //присоединение к игре
+        const {sessionName, user} = data;
+        if (!rooms.has(sessionName)) { console.log(`ROOM ${sessionName} IS NOT CREATED!!!`) } //проверка имени комнаты
+        else { if (rooms.get(sessionName).users.find(function(user) { if (user.name === this.name) {return true} }, user)) { // проверка что такого пользователя нет в комнате
+          console.log(`USER ${user.name} IS ALREADY IN ROOM!`) }
+        else {
+          const roomInfo = rooms.get(sessionName);
+          user.id = socket.id;
+          user.status = 'connect';
+          roomInfo.users.push(user);
+          rooms.set(sessionName, roomInfo);
+          socket.join(sessionName);
+          console.log(rooms.get(sessionName).users)
+        }
+        }
+      })
+
     } catch (error) {
         console.log(error);
-      //return;
     }
 
  })
