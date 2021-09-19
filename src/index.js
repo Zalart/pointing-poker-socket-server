11cca95ba5f26f281c05e081d59fdc6e9e036ecf
artur <me@zalart.com>
2021-09-19 18:50:08 +0300

    feat: redirect data to lobby component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 import {Server} from 'socket.io';
 
-import {userEvents} from '../src/users/userEvents';
+import {gameEvents} from './games/gameEvents';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -13,7 +13,7 @@ const io = new Server(server);
 
 app.use(cors());
 
-userEvents(io);
+gameEvents(io);
 
 
 app.get('/', (req, res) => {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,7 +8,15 @@ export const DISCONNECT = 'disconnect';
  * Custom socket.io events
  */
 
- export const JOIN_ROOM = 'join_room';
+ 
+ export const CONNECT_LOBBY = 'connect_lobby';
+ export const GET_GAME_DATA = "get_game_data";
+ export const GAME_DATA = "game_data";
+ export const GET_GAMES_LIST = "get_games_list";
+ export const GAMES_LIST = "games_list";
+ export const LEAVE_GAME = 'leave_game';
+ export const JOIN_GAME = 'join_game';
+ export const LOBBY_CONNECTED = "lobby_connected";
  export const SEND_MESSAGE = 'send_message';
  export const RECEIVE_MESSAGE = 'receive_message';
  export const USER_CONNECTED = 'user_connected';
