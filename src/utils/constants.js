9428ad15bdbb21d62dcc74db2aaece1ee3c7fc97
artur <me@zalart.com>
2021-10-06 01:41:58 +0300

    chat implemented

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,33 +1,35 @@
 /**
  * Standard socket.io events
  */
-export const CONNECTION = 'connection';
-export const DISCONNECT = 'disconnect';
-export const DISCONNECTING = 'disconnecting';
+export const CONNECTION = "connection";
+export const DISCONNECT = "disconnect";
+export const DISCONNECTING = "disconnecting";
 
 /**
  * Custom socket.io events
  */
 
- export const CONNECT_LOBBY = 'connect_lobby';
- export const GET_GAME_DATA = "get_game_data";
- export const GAME_DATA = "game_data";
- export const GET_GAMES_LIST = "get_games_list";
- export const GAMES_LIST = "games_list";
- export const LEAVE_GAME = 'leave_game';
- export const JOIN_GAME = 'join_game';
- export const LOBBY_CONNECTED = "lobby_connected";
- export const SEND_MESSAGE = 'send_message';
- export const RECEIVE_MESSAGE = 'receive_message';
- export const USER_CONNECTED = 'user_connected';
- export const USER_DISCONNECTED = 'user_disconnected';
- export const KICK_PLAYER = 'kick_player';
- export const BLOCK_APP = 'block_app';
- export const SET_GAME_DATA = 'set_game_data';
- export const START_GAME = 'start_game';
- export const GAME_STARTED = 'game_started';
- export const GET_PLAY_GAME_DATA = 'get_play_game_data';
- export const PLAY_GAME_DATA = 'play_game_data';
- export const RESULTS_DATA = 'resuts_data';
-
-
+export const CONNECT_LOBBY = "connect_lobby";
+export const GET_GAME_DATA = "get_game_data";
+export const GAME_DATA = "game_data";
+export const GET_GAMES_LIST = "get_games_list";
+export const GAMES_LIST = "games_list";
+export const LEAVE_GAME = "leave_game";
+export const JOIN_GAME = "join_game";
+export const LOBBY_CONNECTED = "lobby_connected";
+export const SEND_MESSAGE = "send_message";
+export const RECEIVE_MESSAGE = "receive_message";
+export const USER_CONNECTED = "user_connected";
+export const USER_DISCONNECTED = "user_disconnected";
+export const KICK_PLAYER = "kick_player";
+export const BLOCK_APP = "block_app";
+export const SET_GAME_DATA = "set_game_data";
+export const START_GAME = "start_game";
+export const GAME_STARTED = "game_started";
+export const GET_PLAY_GAME_DATA = "get_play_game_data";
+export const PLAY_GAME_DATA = "play_game_data";
+export const RESULTS_DATA = "resuts_data";
+// ====== chat
+export const SEND_CHAT_MESSAGE = "send_chat_message";
+export const CHAT_MESSAGES = "chat_messages";
+export const GET_CHAT_MESSAGES = "get_chat_messages";
